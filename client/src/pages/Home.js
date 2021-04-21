135beb5ac198805797477fc6dd98d322c5489b8a
njazgunovich <nicholasjaz@gmail.com>
2021-04-20 20:09:11 -0400

    made changes to resoures js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,27 @@
 import React from "react"
-
-
+import ListResources from "../components/ListResources"
+import resources from "../resources"
+var colors=["red","blue","green","orange"]
 function Home(){
+    console.log (resources)
     return (
         <div>
             <h1>Home</h1>
+            <div class ="container-fluid">
+                <div class = "row">
+                    <div class = "col-md-2"></div>
+                    <div class = "col-md-8"></div>
+                    <div class = "col-md-2">
+                        {resources.map((resource,index) => {
+                            console.log(resource)
+                            return (
+                            <ListResources currentColor={colors[index]} resourceList={resource.resources} title={resource.title}/>
+                        )})}
+                
+                    </div>
+                </div>
+            </div>
+            
         </div>
     )
 }
---
+++ client/src/resources.js
@@ -0,0 +1,119 @@
+export default [
+    {
+        "title":"Resources-Code-Practice",
+        "resources":[{
+            link:"https://www.freecodecamp.org/",
+            displayName:"Free code camp"
+        },{
+            link: "https://coderbyte.com",
+            displayName:"coder byte"
+        },{
+            link: "https://www.datacamp.com",
+            displayName:"data camp"
+        },{
+            link: "https://www.hackerrank.com",
+            displayName:"hacker rank"
+        },{
+            link: "https://www.hackerearth.com",
+            displayName:"hacker earth"
+        },{
+            link: "https://www.codingame.com",
+            displayName:"coding game"
+        },{
+            link: "https://www.topcoder.com",
+            displayName:"top coder",
+            fontColor:"red"
+        }
+    
+               
+            ]
+    },
+    {
+        "title":"Job Resources",
+        "resources":[{
+            link:"https://www.linkedin.com/",
+            displayName:"Linkedin"
+        },{
+            link: "https://www.dice.com",
+            displayName:"dice"
+        },{
+            link: "https://www.techcareers.com",
+            displayName:"tech careers"
+        },{
+            link: "https://talent500.co",
+            displayName:"talent 500"
+        },{
+            link: "https://stackoverflow.com/jobs",
+            displayName:"stackoverflow jobs"
+        },{
+            link: "https://www.themuse.com",
+            displayName:"the muse"
+        },{
+            link: "https://www.indeed.com/q-Prime-jobs.html */",
+            displayName:"indeed prime jobs"
+        }
+    
+               
+            ]
+    },
+    {
+        "title":"Free and Inexpensive Code Courses",
+        "resources":[{
+            link:"https://www.codecademy.com",
+            displayName:"codecademy"
+        },{
+            link: "https://www.udemy.com",
+            displayName:"udemy"
+        },{
+            link: "https://teamtreehouse.com",
+            displayName:"team treehouse"
+        },{
+            link: "https://www.freecodecamp.org",
+            displayName:"free code camp"
+        },{
+            link: "https://www.edx.org",
+            displayName:"edx"
+        },{
+            link: "https://alison.com",
+            displayName:"alison"
+        },{
+            link: "https://www.w3schools.com",
+            displayName:"W3 schools"
+        },{
+            link: "https://www.tutorialspoint.com/index.htm",
+            displayName:"tutorials point"
+        },{
+            link: "https://www.datacamp.com",
+            displayName:"datacamp",
+            fontColor:"red"
+        },
+        ]
+    },
+        {
+            "title":"Coding Groups",
+            "resources":[{
+                link:"https://www.meetup.com/",
+                displayName:"meet up"
+            },{
+                link:"https://www.meetup.com/OperationCode_NYC",
+                displayName:" meetup operation code nyc"
+            },{
+                link: "https://www.datacamp.com",
+                displayName:"data camp"
+            },{
+                link: "https://sites.google.com/a/nypl.org/techconnect/project_-code-1",
+                displayName:"new public library techconnect"
+            },{
+                link: "https://ttp.nyc/web-development-fellowship */} */",
+                displayName:"NYC web development fellowship"
+            },{
+                link: "https://www.codingame.com",
+                displayName:"coding game"
+            },{
+                link: "https://https://codebuddies.org/",
+                displayName:"code buddies",
+                fontColor:"red"
+            }
+        ]
+        }
+]
