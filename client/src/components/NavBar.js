625c055fa60eeabb4d88e599c4b2f77ba576ee42
Vivianne Cardillo <viviannecardillo@Viviannes-MBP.fios-router.home>
2021-04-22 18:33:40 -0400

    testing around Signup - delete

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,12 +3,20 @@ import {Link} from "react-router-dom"
 // import {NavLink} from "react-router-dom"
 import "./NavBar.css";
 
+// function NavBar(){
+//     return <nav className="navbar bg-dark link">
+//         <h4><Link className="nav-link link" to="/" ></Link>Home/SignUp Route</h4>
+//         <h4><Link className="nav-link link" to="/blognoteadd"></Link>Blog - Add Note</h4>
+//         <h4><Link className="nav-link link" to="/blognotesall"></Link>Blog - All Notes</h4>
+//         <h4><Link className="nav-link link" to="/resources"></Link>Resources</h4>
+//     </nav>
+
 function NavBar(){
     return <nav className="navbar bg-dark link">
-        <h4><Link className="nav-link link" to="/" ></Link>Home/SignUp Route</h4>
-        <h4><Link className="nav-link link" to="/blognoteadd"></Link>Blog - Add Note</h4>
-        <h4><Link className="nav-link link" to="/blognotesall"></Link>Blog - All Notes</h4>
-        <h4><Link className="nav-link link" to="/resources"></Link>Resources</h4>
+        <h4><Link className="nav-link link" to="/" >Home/SignUp Route</Link></h4>
+        <h4><Link className="nav-link link" to="/blognoteadd">Blog - Add Note</Link></h4>
+        <h4><Link className="nav-link link" to="/blognotesall">Blog - All Notes</Link></h4>
+
     </nav>
 
 
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,11 +1,11 @@
 
 
 const router = require("express").Router();
-const signUpRoutes = require("./signUps");
+// const signUpRoutes = require("./signUps");
 const gitHubUsersRepoRoutes = require("./gitHubUsersRepo");
 
 // signup route
-router.use("/signUps", signUpRoutes);
+// router.use("/signUps", signUpRoutes);
 
 
 // signup route
