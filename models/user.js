752a700d0c3924e885ac085965b5c6bc8b8eede2
Vivianne Cardillo <viviannecardillo@Viviannes-MBP.fios-router.home>
2021-04-24 15:37:47 -0400

    updated user.js model in effort to get new registered users updated on Atlasvia deployment on Heroku.  It works on localhost, but not on Heroku.  Ironically the noteModel.js updating with a new notesSchema update works fine

--- models/user.js
+++ models/user.js
@@ -10,7 +10,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const usersSchema = new Schema({
     username: String,
-    password: String,
+    password: String
 
 })
 const User = mongoose.model("User", usersSchema);
