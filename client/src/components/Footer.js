081b1d6c3e0876bcc4c8d908b0f708ccfcaf83ca
Natalia Carvajal <nataliacarvajal1996@gmail.com>
2021-04-26 01:52:15 -0400

    footer complete

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,9 +3,9 @@ import "./Footer.css";
 function Footer() {
   return (
     <div>
-      <div className="footer">
+      <div className="footer text-center">
         {" "}
-        Made with <i class="far fa-heart"></i>
+        Made with <i class="far fa-heart fa-lg"></i>
       </div>
     </div>
   );
