78d84a597a65d18d8e1787a87a702241c0562dc8
Vivianne Cardillo <viviannecardillo@Viviannes-MBP.fios-router.home>
2021-04-26 00:19:52 -0400

    commit to deploy

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -16,7 +16,7 @@ import "./NavBar.css";
 function NavBar(props){
     return <nav className="navbar bg-dark link">
         <h4><Link className="nav-link link" to="/" >Home/SignUp Route</Link></h4>
-        {/* <h4><Link className="nav-link link" to="/api" >SignUp Route</Link></h4> */}
+        
         <h4><Link className="nav-link link" to="/blognoteadd">Blog - Add Note</Link></h4>
         <h4><Link className="nav-link link" to="/blognotesall">Blog - All Notes</Link></h4>
    <div>
